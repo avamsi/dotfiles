cdaa352b4f7f398ae6d47af60beadc99f2cb70a4
Vamsi Krishna Avula <vamsi_ism@outlook.com>
2020-09-06 17:44:31 +0530

    Create aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,25 @@
+alias ...='cd ../..'
+alias ..='cd ..'
+alias files='nautilus'
+alias grep='grep --color'
+alias ll='ls -al'
+alias ls='ls --classify --color'
+alias re='grep -inrI'
+
+td() {
+  local session
+  session=$1
+  shift
+  tmux new-session -s $session -d 2> /dev/null && {
+    if [[ "$#" -ne 0 ]]; then
+      tmux send-keys -t $session $@ C-m
+    fi
+  }
+  tmux attach-session -t $session 2> /dev/null || tmux switch-client -t $session
+}
+
+tmicro() {
+  tmux source ~/.tmux/reset_keys.conf
+  ~/bin/micro $@
+  tmux source ~/.tmux/keys.conf
+}
