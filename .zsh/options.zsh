4eb4abb0efbd5879ee671f2cfdfb6030694206a2
Vamsi Avula <vamsi_ism@outlook.com>
2021-08-01 15:27:03 +0530

    long pending options fix

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -1,36 +1,19 @@
 # Options -- http://zsh.sourceforge.net/Doc/Release/Options.html
 
-
 # Changing Directories
-
 setopt AUTO_CD
 
-
 # Completion
-
 setopt AUTO_LIST
 setopt AUTO_MENU
 setopt LIST_TYPES
 
-
 # History
-
-setopt EXTENDED_HISTORY
-setopt INC_APPEND_HISTORY
 setopt HIST_FCNTL_LOCK
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_FIND_NO_DUPS
-setopt HIST_IGNORE_DUPS
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
 setopt SHARE_HISTORY
 setopt HIST_BEEP
 
-
 # Input/Output
-
 setopt CORRECT
-
-
-# Prompting
-setopt PROMPT_SUBST
