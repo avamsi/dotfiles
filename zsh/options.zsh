6c74e85f5dacd1c5770f78a70c919c8fd792b868
Vamsi Avula <avamsi07@gmail.com>
2023-01-03 22:44:08 +0530

    update options.zsh to remove some that are redundant and unset CASE_GLOB

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,19 +1,17 @@
-# Options -- http://zsh.sourceforge.net/Doc/Release/Options.html
+# http://zsh.sourceforge.net/Doc/Release/Options.html
 
 # Changing Directories
 setopt AUTO_CD
 
-# Completion
-setopt AUTO_LIST
-setopt AUTO_MENU
-setopt LIST_TYPES
+# Expansion and Globbing
+unsetopt CASE_GLOB
 
 # History
 setopt HIST_FCNTL_LOCK
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
 setopt SHARE_HISTORY
-setopt HIST_BEEP
 
 # Input/Output
 setopt CORRECT
+setopt CORRECT_ALL
