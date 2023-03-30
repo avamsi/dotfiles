569dd4db83fc0e19ddb9e30d67d9b9f86491d50a
Vamsi Avula <avamsi07@gmail.com>
2023-03-31 05:27:11 +0530

    add the note about local in aliases.zsh as well

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,6 +13,8 @@ alias sudo='sudo '
 cd() {
 	# Keep the original cd error hidden for if rd succeeds below.
 	builtin cd $@ 2>/tmp/rd-cde || {
+		# Note: don't try to "fix" this by adding `local`, it doesn't work.
+		# For whatever reason, the `&&` chaining behaviour is lost with it.
 		d=$(rd $@) && builtin cd $d || {
 			# No luck, show the original error as well.
 			cat /tmp/rd-cde
