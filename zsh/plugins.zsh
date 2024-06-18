a9b4f219276d8f10dd42a4862aec4d826558888d
Vamsi Avula <avamsi07@gmail.com>
2024-06-18 23:44:46 +0530

    cleanup plugins.zsh slightly to work on mac

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,15 +1,20 @@
 if [[ -z "$HOMEBREW_PREFIX" ]]; then
-    return
+	return
 fi
 
 source "$HOMEBREW_PREFIX/share/antigen/antigen.zsh"
 
 antigen theme romkatv/powerlevel10k
 
-antigen bundle Aloxaf/fzf-tab; zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+antigen bundle Aloxaf/fzf-tab
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
 antigen bundle Valiev/almostontop
-antigen bundle hlissner/zsh-autopair; unset 'AUTOPAIR_PAIRS[\`]'
+antigen bundle hlissner/zsh-autopair
 antigen bundle zdharma-continuum/fast-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 
 antigen apply
+
+unset 'AUTOPAIR_PAIRS[`]'
